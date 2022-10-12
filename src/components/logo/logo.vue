5aacb2c9f2c17a3787fd4b4f783cf47390e0b878
Tim Cook <timleadofficial@gmail.com>
2022-10-12 10:45:29 +0300

    Added components to the storybook

---
+++ src/components/logo/logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="logo">
+    <a
+      href="https://loftschool.com"
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <Icon name="logo" />
+    </a>
+  </div>
+</template>
+
+<script>
+import Icon from '../../icons/icon.vue'
+export default {
+  components: {
+    Icon
+  }
+}
+</script>
+
+<style src="./logo.scss" lang="scss" scoped></style>
---
+++ src/components/navigation/navigation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="user-info">
+    <div class="home">
+      <Icon name="home" />
+    </div>
+    <div class="user-photo">
+      <img
+        src="https://shapka-youtube.ru/wp-content/uploads/2020/07/letter-c.jpg"
+        class="img-avatar"
+        alt="user photo"
+      />
+    </div>
+    <div class="logout">
+      <Icon name="logout" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Icon from '../../icons/icon.vue'
+export default {
+  components: {
+    Icon
+  }
+}
+</script>
+
+<style src="./navigation.scss" lang="scss" scoped></style>
